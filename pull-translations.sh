9486410bc45eb64ec980f173da4fc6c81e83e2ed
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-10-07 14:50:38 +0200

    feat: test

---
+++ pull-translations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BRANCH_NAME=${1:-master}
+
+# Pull translations
+echo "🛠️ Pulling translations from $BRANCH_NAME branch"
+lokalise2 --token 4dd34462a622a8d9a294feeddf976bc017d93a33 --project-id 108144176703c7e6803125.68848339:$BRANCH_NAME file download --unzip-to "./locales" --format "json" --export-sort "first_added"
